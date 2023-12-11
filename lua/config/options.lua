08d065af25299a14fe19217a94bb8dfd9cce3c64
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-11 06:35:36 -0700

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- vim options
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 0
+vim.opt.writebackup = true
+vim.opt.wrap = true
+vim.opt.guicursor = "i:block"
+vim.opt.cmdheight = 0 -- 0 for noice.nvim
+vim.opt.conceallevel = 1 -- enable for VimTex
+-- vim.opt.wrapmargin = 100
+-- vim.opt.foldlevel = 1
+-- vim.opt.foldclose = "all"
+
+-- Ensure gx is working
+-- vim.cmd("packadd! netrw")
