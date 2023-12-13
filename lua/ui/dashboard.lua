a3e181d2db0e3094a4b677c4ca9a173baacb1e64
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-12 20:40:58 -0700

    Update Alpha settings

--- lua/ui/dashboard.lua
+++ lua/ui/dashboard.lua
@@ -1,7 +1,23 @@
 return {
 	"goolord/alpha-nvim",
-  enabled = true,
+	enabled = true,
+
 	event = "VimEnter",
+	cmd = "Alpha",
+
+	opts = function()
+		local dashboard = require("alpha.themes.dashboard")
+		local logo = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+    ]]
+		dashboard.section.header.val = vim.split(logo, "\n")
+	end,
+
 	config = function()
 		require("alpha").setup(require("alpha.themes.dashboard").config)
 	end,
