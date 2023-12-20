5c181ab579355f85f56a3c08f2049ea94ba7a262
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-20 04:09:14 -0700

    Update options.lua, grouping opts into categories

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,19 +1,30 @@
--- vim options
+-- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- coding options
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 0
+
+-- editing options
+vim.opt.autowrite = true
 vim.opt.writebackup = true
-vim.opt.wrap = true
-vim.opt.guicursor = "i:block"
+
+-- UI options
+vim.opt.scrolloff = 0
 vim.opt.cmdheight = 0 -- 0 for noice.nvim
 vim.opt.conceallevel = 1 -- enable for VimTex
+vim.opt.wrap = true
+vim.opt.guicursor = "i:block"
+vim.opt.fillchars = {
+	eob = " ",
+}
+
+-- options to consider in the future
 -- vim.opt.wrapmargin = 100
 -- vim.opt.foldlevel = 1
 -- vim.opt.foldclose = "all"
 
-vim.opt.fillchars = {
-	eob = " ",
-}
 -- Ensure gx is working
 -- vim.cmd("packadd! netrw")
