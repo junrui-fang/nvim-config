cbdb488a6831eca6c63b9d76a42db3d64d8caca3
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-20 21:46:07 -0700

    Basic customization to lualine

--- lua/ui/lines.lua
+++ lua/ui/lines.lua
@@ -8,7 +8,38 @@ return {
 
 		event = "VeryLazy",
 
-		opts = {},
+		opts = {
+			options = {
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = {
+					statusline = { "alpha" },
+					winbar = {},
+				},
+			},
+
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "diff", "diagnostics" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "diff", "diagnostics" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = { "lazy", "neo-tree" },
+		},
 	},
 
 	-- Buffer line
