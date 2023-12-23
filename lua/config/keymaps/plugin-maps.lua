ec0da58d7840d9283fcc86784ed3d371462846d3
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-22 22:29:43 -0700

    Add keymaps for zen mode

--- lua/config/keymaps/plugin-maps.lua
+++ lua/config/keymaps/plugin-maps.lua
@@ -1,6 +1,18 @@
 local wk = require("which-key")
 
--- neo-tree keymaps
+-- File Explorer
 wk.register({
 	["<leader>e"] = { "<cmd>Neotree toggle<cr>", "File Explorer" },
 })
+
+-- Zen Mode
+wk.register({
+	["<leader>z"] = { "<cmd>ZenMode<cr>", "Zen Mode" },
+})
+
+-- Buffer
+-- Terminal
+-- Search
+-- Notification
+-- Package Management
+-- Git
