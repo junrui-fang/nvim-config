562bcc90e17d56d867121fb5e6596b3078efa9a7
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-29 13:55:13 -0700

    Add keymaps for windows navigation

--- lua/config/keymaps/system-maps.lua
+++ lua/config/keymaps/system-maps.lua
@@ -6,6 +6,11 @@ vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>c", "<cmd>bd<cr>", { desc = "Close" })
 
 -- Windows
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
 -- Navigation
 -- Editing
 -- LSP
