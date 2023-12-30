be37509c792592c1adb243be7a5cc6038e4ca5a6
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-30 14:26:55 -0700

    Update config of terminal (toggleterm.nvim)

--- lua/core/terminal.lua
+++ lua/core/terminal.lua
@@ -4,8 +4,29 @@ return {
 		version = "*",
 
 		cmd = { "ToggleTerm", "TermExec" },
-    keys = {},
+		keys = {
+			"<C-'>",
+			{ "<M-1>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizional Terminal" },
+			{ "<M-2>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertical Terminal" },
+			{ "<M-3>", "<cmd>ToggleTerm direction=float<cr>", desc = "Float Terminal" },
+		},
 
-		opts = {},
+		opts = {
+			open_mapping = [[<c-'>]],
+			autochdir = true,
+			persist_mode = true,
+
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.33
+				end
+			end,
+
+			float_opts = {
+				border = "curved",
+			},
+		},
 	},
 }
