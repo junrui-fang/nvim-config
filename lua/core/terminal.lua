86866187512f248aab33782cb6c622d2e15e224a
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2023-12-31 10:30:28 -0700

    Setup toggleterm to use lazygit

--- lua/core/terminal.lua
+++ lua/core/terminal.lua
@@ -4,12 +4,6 @@ return {
 		version = "*",
 
 		cmd = { "ToggleTerm", "TermExec" },
-		keys = {
-			"<C-'>",
-			{ "<M-1>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizional Terminal" },
-			{ "<M-2>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertical Terminal" },
-			{ "<M-3>", "<cmd>ToggleTerm direction=float<cr>", desc = "Float Terminal" },
-		},
 
 		opts = {
 			open_mapping = [[<c-'>]],
@@ -26,6 +20,41 @@ return {
 
 			float_opts = {
 				border = "curved",
+			},
+		},
+
+		keys = {
+			"<C-'>",
+			{ "<M-1>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizional Terminal" },
+			{ "<M-2>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertical Terminal" },
+			{ "<M-3>", "<cmd>ToggleTerm direction=float<cr>", desc = "Float Terminal" },
+
+			-- lazygit
+			{
+				"<leader>gg",
+
+				-- https://github.com/LunarVim/LunarVim/blob/b124e8c3e3f8145029c0d9aeb3912e5ac314e0a2/lua/lvim/core/terminal.lua#L149
+				function()
+					local Terminal = require("toggleterm.terminal").Terminal
+					local lazygit = Terminal:new({
+						cmd = "lazygit",
+						hidden = true,
+						direction = "float",
+						float_opts = {
+							border = "none",
+							width = 100000,
+							height = 100000,
+						},
+						on_open = function(_)
+							vim.cmd("startinsert!")
+						end,
+						on_close = function(_) end,
+						count = 99,
+					})
+					lazygit:toggle()
+				end,
+
+				desc = "Lazygit",
 			},
 		},
 	},
