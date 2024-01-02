14d35676995c9adde1d1a7f22fc7da7d6bad2c07
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-01 23:19:45 -0700

    Update none-ls setup

--- lua/lsp/lsp-integrator.lua
+++ lua/lsp/lsp-integrator.lua
@@ -6,9 +6,18 @@ return {
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+				-- Formatters
+				null_ls.builtins.formatting.stylua, -- lua
+				null_ls.builtins.formatting.black, -- Python
+				null_ls.builtins.formatting.isort, -- Python
+				null_ls.builtins.formatting.prettier, -- formatter for many types of files
+
 				null_ls.builtins.completion.spell,
 			},
 		})
 	end,
+
+	keys = {
+		{ "<leader>af", vim.lsp.buf.format, desc = "Format" },
+	},
 }
