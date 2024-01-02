262b3bc29d58d0c7a582ed80be267a9666ad5ee0
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-02 15:01:13 -0700

    Update dashboard

--- lua/ui/dashboard.lua
+++ lua/ui/dashboard.lua
@@ -1,24 +1,53 @@
 return {
 	"goolord/alpha-nvim",
-	enabled = true,
-
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VimEnter",
-	cmd = "Alpha",
-
-	opts = function()
-		local dashboard = require("alpha.themes.dashboard")
-		local logo = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-    ]]
-		dashboard.section.header.val = vim.split(logo, "\n")
-	end,
 
 	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Logo
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+
+		-- Buttons
+		dashboard.section.buttons.val = {
+			dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+			dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+			dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+			dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+			-- dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+			dashboard.button("p", " " .. " Plugins", "<cmd> Lazy <cr>"),
+			dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+		}
+
+		-- Highlight groups
+		for _, button in ipairs(dashboard.section.buttons.val) do
+			button.opts.hl = "AlphaButtons"
+			button.opts.hl_shortcut = "AlphaShortcut"
+		end
+		dashboard.section.header.opts.hl = "AlphaHeader"
+		dashboard.section.buttons.opts.hl = "AlphaButtons"
+		dashboard.section.footer.opts.hl = "AlphaFooter"
+
+		-- Layout
+		dashboard.opts.layout[1].val = 8
+
+		require("alpha").setup(dashboard.opts)
 	end,
 }
