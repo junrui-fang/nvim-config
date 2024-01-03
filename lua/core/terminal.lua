87ce86140e3e3ae393acd844779b7b4ba34e90c9
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-03 14:59:13 -0700

    Move lazygit settings from terminal.lua to git/lazygit.lua

--- lua/core/terminal.lua
+++ lua/core/terminal.lua
@@ -28,34 +28,6 @@ return {
 			{ "<M-1>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizional Terminal" },
 			{ "<M-2>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertical Terminal" },
 			{ "<M-3>", "<cmd>ToggleTerm direction=float<cr>", desc = "Float Terminal" },
-
-			-- lazygit
-			{
-				"<leader>gg",
-
-				-- https://github.com/LunarVim/LunarVim/blob/b124e8c3e3f8145029c0d9aeb3912e5ac314e0a2/lua/lvim/core/terminal.lua#L149
-				function()
-					local Terminal = require("toggleterm.terminal").Terminal
-					local lazygit = Terminal:new({
-						cmd = "lazygit",
-						hidden = true,
-						direction = "float",
-						float_opts = {
-							border = "none",
-							width = 100000,
-							height = 100000,
-						},
-						on_open = function(_)
-							vim.cmd("startinsert!")
-						end,
-						on_close = function(_) end,
-						count = 99,
-					})
-					lazygit:toggle()
-				end,
-
-				desc = "Lazygit",
-			},
 		},
 	},
 }
