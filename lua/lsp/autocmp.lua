550839a52453e15129a6eb195e2227f038a12f6e
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-03 20:28:26 -0700

    Disable auto completion for cmdline

--- lua/lsp/autocmp.lua
+++ lua/lsp/autocmp.lua
@@ -72,14 +72,14 @@ return {
 			})
 
 			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-			})
+			-- cmp.setup.cmdline(":", {
+			-- 	mapping = cmp.mapping.preset.cmdline(),
+			-- 	sources = cmp.config.sources({
+			-- 		{ name = "path" },
+			-- 	}, {
+			-- 		{ name = "cmdline" },
+			-- 	}),
+			-- })
 
 			-- Insert `(` after select function or method item
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
