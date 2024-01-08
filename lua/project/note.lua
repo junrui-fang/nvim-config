f18100ae3d8b144fa892c90a83fd4f9dd86d007e
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-08 02:38:03 -0700

    Update Neorg config

--- lua/project/note.lua
+++ lua/project/note.lua
@@ -12,24 +12,12 @@ return {
 				load = {
 					["core.defaults"] = {},
 					["core.concealer"] = {},
+
 					["core.dirman"] = {
 						config = {
 							workspaces = {
-								study = "/Users/junruifang/Projects/Note/notes/study/",
-								English = "/Users/junruifang/Projects/Note/notes/study/English/",
-								Vim = "/Users/junruifang/Projects/Note/notes/study/Vim/",
-								Git = "/Users/junruifang/Projects/Note/notes/study/Git/",
-								CLI = "/Users/junruifang/Projects/Note/notes/study/CLI/",
-								Java = "/Users/junruifang/Projects/Note/notes/study/Java/",
-								Python = "/Users/junruifang/Projects/Note/notes/study/Python/",
-								HTML = "/Users/junruifang/Projects/Note/notes/Frontend/HTML/",
-								CSS = "/Users/junruifang/Projects/Note/notes/Frontend/CSS/",
-								work = "/Users/junruifang/Projects/Note/notes/work/",
-								Workflow = "/Users/junruifang/Projects/Note/notes/Workflow/",
-								auto = "/Users/junruifang/Projects/Note/notes/work/auto",
-								life = "/Users/junruifang/Projects/Note/notes/life/",
+								notes = "~/Projects/Note/notes/",
 							},
-							default_workspace = "study",
 						},
 					},
 				},
@@ -37,5 +25,9 @@ return {
 			vim.wo.foldlevel = 99
 			vim.wo.conceallevel = 2
 		end,
+
+		keys = {
+			{ "<leader>pn", "<cmd>Neorg index<cr>", desc = "Notes" },
+		},
 	},
 }
