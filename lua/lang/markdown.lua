04d229b65c4c9dddb8b86325ae3d160d3971c2a9
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-24 23:47:04 -0700

    Config markdown-preview

--- lua/lang/markdown.lua
+++ lua/lang/markdown.lua
@@ -7,5 +7,14 @@ return {
 
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
+
+		config = function()
+			vim.g.mkdp_auto_close = 1
+			vim.g.mkdp_theme = "dark"
+		end,
+
+		keys = {
+			{ "<leader>m", "<cmd>MarkdownPreview<cr>", ft = "markdown", desc = "Markdown Preview" },
+		},
 	},
 }
