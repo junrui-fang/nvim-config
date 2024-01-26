38eec9bd8de35d9aec2c46456fdeca21b06d739e
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-26 00:30:03 -0700

    Install fienvim

---
+++ lua/tools/browser.lua
@@ -0,0 +1,10 @@
+return {
+	-- Turn browser into a Neovim client
+	{
+		"glacambre/firenvim",
+		lazy = not vim.g.started_by_firenvim,
+		build = function()
+			vim.fn["firenvim#install"](0)
+		end,
+	},
+}
