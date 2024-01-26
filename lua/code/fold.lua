ca231b089576a5b8ec72cd34174b3ff4392eed6d
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-25 23:06:00 -0700

    Install nvim-ufo as code folding tool

---
+++ lua/code/fold.lua
@@ -0,0 +1,45 @@
+return {
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+
+		config = function()
+			vim.o.foldcolumn = "0"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			require("ufo").setup({
+				close_fold_kinds = { "imports", "comment" },
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+
+				preview = {
+					win_config = {
+						border = { "", "─", "", "", "", "─", "", "" },
+						winhighlight = "Normal:Folded",
+						winblend = 0,
+					},
+					mappings = {
+						scrollU = "<C-u>",
+						scrollD = "<C-d>",
+						jumpTop = "[",
+						jumpBot = "]",
+					},
+				},
+			})
+
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+			vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+			vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+			vim.keymap.set("n", "K", function()
+				local winid = require("ufo").peekFoldedLinesUnderCursor()
+				if not winid then
+					vim.lsp.buf.hover()
+				end
+			end)
+		end,
+	},
+}
