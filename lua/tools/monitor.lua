9f571e779250cca57b4c3da9791150aebe461486
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-26 16:33:05 -0700

    Integrate Bottom as the system monitor

---
+++ lua/tools/monitor.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		keys = {
+			-- Integrate Bottom as the system monitor
+			{
+				"<leader>tm",
+
+				function()
+					local Terminal = require("toggleterm.terminal").Terminal
+
+					local bottom = Terminal:new({
+						cmd = "btm",
+						hidden = true,
+						direction = "float",
+						close_on_exit = true,
+					})
+					bottom:toggle()
+				end,
+
+				desc = "Monitor",
+			}
+		},
+	},
+}
