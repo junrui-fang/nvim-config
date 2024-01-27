f139a51013c55eccd7de712d45c2bc309c6d210e
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-01-26 17:18:45 -0700

    Integrate navi as the cheatsheet

---
+++ lua/tools/cheatsheet.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		keys = {
+			-- Integrate navi as the cheatsheet
+			{
+				"<leader>tc",
+
+				function()
+					local Terminal = require("toggleterm.terminal").Terminal
+
+					local navi = Terminal:new({
+						cmd = "navi",
+						hidden = true,
+						direction = "float",
+						close_on_exit = true,
+					})
+
+					navi:toggle()
+				end,
+
+				desc = "Cheatsheet",
+			}
+		},
+	},
+}
