70be793e846985a0aa57b0e1c1dd87b529067f9d
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-02-08 16:57:13 -0700

    Refactor system keymaps

--- lua/config/keymaps/system-maps.lua
+++ lua/config/keymaps/system-maps.lua
@@ -1,42 +1,44 @@
+local keymap = vim.keymap.set
+
 -- <space> does nothing in normal & visual mode
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+keymap({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Clear search with <esc>
-vim.keymap.set("n", "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+keymap("n", "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
 -- Easier closing & quitting
-vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>", { desc = "Quit" })
-vim.keymap.set("n", "<leader>c", "<cmd>bd<cr>", { desc = "Close" })
+keymap("n", "<leader>q", "<cmd>confirm q<cr>", { desc = "Quit" })
+keymap("n", "<leader>c", "<cmd>bd<cr>", { desc = "Close" })
 
 -- Windows
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+keymap("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 -- Tabs
-vim.keymap.set("n", "<leader><tab>j", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader><tab>k", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
-vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-vim.keymap.set("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+keymap("n", "<leader><tab>j", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+keymap("n", "<leader><tab>k", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+keymap("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+keymap("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+keymap("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+keymap("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
 -- Terminal
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
-vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+keymap("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+keymap("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+keymap("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
 
 -- Editing
-vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Cursor move right" })
-vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+keymap("i", "<C-l>", "<Right>", { desc = "Cursor move right" })
+keymap("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+keymap("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+keymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+keymap("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+keymap("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Navigation
-vim.keymap.set("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
-vim.keymap.set("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
+keymap("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
+keymap("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
 
 local diagnostic_goto = function(next, severity)
 	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
@@ -45,18 +47,18 @@ local diagnostic_goto = function(next, severity)
 		go({ severity = severity })
 	end
 end
-vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+keymap("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+keymap("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+keymap("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+keymap("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+keymap("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+keymap("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
 -- LSP
--- vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
--- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
--- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
--- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
--- vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto References" })
--- vim.keymap.set({ "n", "v" }, "<leader>aa", vim.lsp.buf.code_action, { desc = "Code Actions" })
--- vim.keymap.set("n", "<leader>ar", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Rename" })
+-- keymap("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+-- keymap("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+-- keymap("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+-- keymap("n", "gi", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+-- keymap("n", "gr", vim.lsp.buf.references, { desc = "Goto References" })
+-- keymap({ "n", "v" }, "<leader>aa", vim.lsp.buf.code_action, { desc = "Code Actions" })
+-- keymap("n", "<leader>ar", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Rename" })
