b7364db177bbff7ff22bda69f3e353939c85b49c
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-04-26 02:41:00 -0600

    Update trouble, now it shows references

--- lua/ui/list.lua
+++ lua/ui/list.lua
@@ -6,7 +6,8 @@ return {
 	opts = {},
 
 	keys = {
-		{ "<leader>lt", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
+		{ "<leader>lr", "<cmd>Trouble lsp_references<cr>", desc = "References" },
+		{ "<leader>lt", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
 		{ "<leader>lT", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
 		-- { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
 		-- { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
