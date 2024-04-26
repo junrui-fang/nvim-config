8fdb9e55f1a9ad3591e92165fb079695dd231673
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-04-26 05:50:28 -0600

    Install a template plugin

---
+++ lua/editor/template.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"cvigilv/esqueleto.nvim",
+
+		ft = {
+			"html",
+			"py",
+			"markdown",
+		},
+
+		opts = {
+			directories = { vim.fn.stdpath("config") .. "/skeletons" },
+			autouse = true,
+
+			patterns = {
+				"LICENSE",
+				"python",
+
+				"html",
+				-- "index.html",
+
+				"markdown",
+			},
+
+			-- Advanced options
+			advanced = {
+				ignored = {}, -- List of glob patterns or function that determines if a file is ignored
+				ignore_os_files = true, -- whether to ignore OS files (e.g. .DS_Store)
+			},
+		},
+	},
+}
