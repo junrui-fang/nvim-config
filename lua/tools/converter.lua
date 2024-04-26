aff7130f77d943b4627a16f02e9eb2c3efe86181
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-04-26 03:07:38 -0600

    Install silicon.nvim as a tool to convert code snippets to images

--- lua/tools/converter.lua
+++ lua/tools/converter.lua
@@ -8,4 +8,21 @@ return {
 			{ "<leader>td", "<cmd>Pandoc<cr>", desc = "Doc Converter" },
 		},
 	},
+
+	-- Code snippet to image
+	{
+		"krivahtoo/silicon.nvim",
+		build = "./install.sh",
+
+		config = function()
+			require("silicon").setup({
+				font = "FantasqueSansMono Nerd Font=16",
+				theme = "Monokai Extended",
+			})
+		end,
+
+		keys = {
+			{ "<leader>ti", mode = "x", ":Silicon ", desc = "Image of code" },
+		},
+	},
 }
