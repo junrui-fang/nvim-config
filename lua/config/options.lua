522ed2b5cf490e3d05ff8d798807291932773ad0
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-06 05:56:47 -0600

    Update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,15 +5,18 @@ vim.g.maplocalleader = "\\"
 -- coding options
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.breakindent = true
 -- vim.opt.expandtab = true -- Use spaces instead of tabs
 
 -- editing options
 vim.opt.autowrite = true
 vim.opt.writebackup = true
+vim.opt.undofile = true
 vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.updatetime = 250 -- Decrease update time
 vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
 vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.inccommand = "nosplit" -- preview incremental substitute
+vim.opt.inccommand = "split" -- preview incremental substitute
 
 -- Opening options
 vim.opt.splitright = true
@@ -21,18 +24,22 @@ vim.opt.splitbelow = true
 vim.opt.splitkeep = "cursor"
 
 -- Search options
+vim.opt.ignorecase = true
 vim.opt.smartcase = true -- Don't ignore case with capitals
 
 -- UI options
 vim.opt.scrolloff = 0
 vim.opt.cmdheight = 0 -- 0 for noice.nvim
 vim.opt.conceallevel = 1 -- enable for VimTex
+vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.showmode = false
 vim.opt.termguicolors = true -- True color support
 vim.opt.cursorline = true -- Enable highlighting of the current line
 vim.opt.wrap = true
 vim.opt.list = true -- Show some invisible characters
 vim.opt.guicursor = "i:block"
+vim.g.have_nerd_font = true
 vim.opt.fillchars = {
 	eob = " ",
 	foldopen = "",
