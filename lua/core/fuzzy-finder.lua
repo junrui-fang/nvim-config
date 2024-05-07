eff5083dbaa0e5ddff52b0b4832c66fb37e9ca7b
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-07 03:09:46 -0600

    Update telescope

--- lua/core/fuzzy-finder.lua
+++ lua/core/fuzzy-finder.lua
@@ -1,15 +1,13 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = "make",
-				config = function()
-					require("telescope").load_extension("fzf")
-				end,
 			},
 		},
 
@@ -30,11 +28,25 @@ return {
 					},
 				},
 			},
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				},
+				-- ["ui-select"] = {
+				-- 	require("telescope.themes").get_dropdown(),
+				-- },
+			},
 		},
 
 		config = function(_, opts)
 			require("telescope").setup(opts)
-			require("telescope").load_extension("projects")
+			pcall(require("telescope").load_extension, "fzf")
+			-- pcall(require("telescope").load_extension, "ui-select")
+			-- require("telescope").load_extension("projects")
 		end,
 
 		keys = {
@@ -67,37 +79,4 @@ return {
 			{ "<leader>bf", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
 		},
 	},
-
-	-- {
-	-- 	"nvim-telescope/telescope-ui-select.nvim",
-	-- 	config = function()
-	-- 		-- This is your opts table
-	-- 		require("telescope").setup({
-	-- 			extensions = {
-	-- 				["ui-select"] = {
-	-- 					require("telescope.themes").get_dropdown({
-	-- 						-- even more opts
-	-- 					}),
-
-	-- 					-- pseudo code / specification for writing custom displays, like the one
-	-- 					-- for "codeactions"
-	-- 					-- specific_opts = {
-	-- 					--   [kind] = {
-	-- 					--     make_indexed = function(items) -> indexed_items, width,
-	-- 					--     make_displayer = function(widths) -> displayer
-	-- 					--     make_display = function(displayer) -> function(e)
-	-- 					--     make_ordinal = function(e) -> string
-	-- 					--   },
-	-- 					--   -- for example to disable the custom builtin "codeactions" display
-	-- 					--      do the following
-	-- 					--   codeactions = false,
-	-- 					-- }
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 		-- To get ui-select loaded and working with telescope, you need to call
-	-- 		-- load_extension, somewhere after setup function:
-	-- 		require("telescope").load_extension("ui-select")
-	-- 	end,
-	-- },
 }
