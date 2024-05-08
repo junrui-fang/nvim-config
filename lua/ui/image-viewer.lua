9ed1f4356f6ca10b007cf00804212e278d066493
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-08 04:03:55 -0600

    Install an image-viewer

---
+++ lua/ui/image-viewer.lua
@@ -0,0 +1,11 @@
+return {
+	"3rd/image.nvim",
+	dependencies = { "luarocks.nvim" },
+
+	config = function()
+		require("image").setup({
+			-- ISSUE: Not working
+			backend = "ueberzug",
+		})
+	end,
+}
