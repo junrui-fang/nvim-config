b45cdc8e01df1d338dfafdcd540349710f957114
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-12 09:28:35 -0600

    Disable vim-pandoc

--- lua/tools/converter.lua
+++ lua/tools/converter.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"vim-pandoc/vim-pandoc",
 		dependencies = { "vim-pandoc/vim-pandoc-syntax" },
-		lazy = false,
+		-- lazy = false,
 
 		keys = {
 			{ "<leader>td", "<cmd>Pandoc<cr>", desc = "Doc Converter" },
