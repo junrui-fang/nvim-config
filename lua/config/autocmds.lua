022001ccd773e30b18352e3d330883f3cdccb3d0
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-12 11:29:06 -0600

    Add an autocmd that create dir when saving file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -52,6 +52,18 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
+-- Auto create intermediate dir when saving a file
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+	callback = function(event)
+		if event.match:match("^%w%w+:[\\/][\\/]") then
+			return
+		end
+		local file = vim.uv.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
+
 -- Close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
 	group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
