5c0d048f399c11110d30f81ca6dcd2eb889e1588
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-14 01:46:23 -0600

    Install nvim-devdocs and update keymaps

--- lua/code/doc.lua
+++ lua/code/doc.lua
@@ -20,10 +20,31 @@ return {
 		},
 
 		keys = {
-			{ "<leader>dd", "<cmd>Neogen func<cr>", desc = "Function" },
+			{ "<leader>df", "<cmd>Neogen func<cr>", desc = "Function" },
 			{ "<leader>dc", "<cmd>Neogen class<cr>", desc = "Class" },
 			{ "<leader>dt", "<cmd>Neogen type<cr>", desc = "Type" },
-			{ "<leader>df", "<cmd>Neogen file<cr>", desc = "File" },
+			{ "<leader>dF", "<cmd>Neogen file<cr>", desc = "File" },
+		},
+	},
+
+	-- DevDocs documentations search
+	{
+		"luckasRanarison/nvim-devdocs",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			after_open = function(bufnr)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":close<CR>", {})
+			end,
+		},
+
+		keys = {
+			{ "<leader>dd", "<cmd>DevdocsToggle<cr>", desc = "Devdocs Toggle" },
+			{ "<leader>ds", "<cmd>DevdocsOpenCurrentFloat<cr>", desc = "Devdocs Search" },
+			{ "<leader>dS", "<cmd>DevdocsOpenFloat<cr>", desc = "Devdocs Search other langs" },
 		},
 	},
 }
