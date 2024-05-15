7622400a9b38f45a003ce560d58c5ea4ba7ed7e8
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-15 05:18:23 -0600

    Create a terminal for gh dash

---
+++ lua/git/github.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		keys = {
+			{
+				"<leader>gd",
+				function()
+					local Terminal = require("toggleterm.terminal").Terminal
+					local lazygit = Terminal:new({
+						cmd = "gh dash",
+						hidden = true,
+						direction = "float",
+						float_opts = {
+							border = "none",
+							width = 100000,
+							height = 100000,
+						},
+						on_open = function(_)
+							vim.cmd("startinsert!")
+						end,
+						on_close = function(_) end,
+						count = 99,
+					})
+					lazygit:toggle()
+				end,
+				desc = "Dashboard (github)",
+			},
+		},
+	},
+}
