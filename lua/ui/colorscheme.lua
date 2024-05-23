018eb977ca0b9dc6153f439f74d9f4bb72bc2df2
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-22 22:18:00 -0600

    Install color highlight plugin

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -10,6 +10,27 @@ return {
     },
   },
 
+  -- Highlight colors, eg: #eb6f92 rgb(166 227 161)
+  { -- Alternative: nvim-highlight-colors
+    "uga-rosa/ccc.nvim",
+    event = { "BufReadPost", "InsertEnter" },
+    cmd = { "CccPick", "CccConvert", "CccHighlighterEnable", "CccHighlighterDisable", "CccHighlighterToggle" },
+
+    opts = {
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    },
+    config = function(_, opts) require("ccc").setup(opts) end,
+
+    keys = {
+      { "<leader>Uh", "<cmd>CccHighlighterToggle<cr>", desc = "Highlight Colors" },
+      { "<leader>Up", "<cmd>CccPick<cr>", desc = "Pick Color" },
+      { "<leader>Uc", "<cmd>CccConvert<cr>", desc = "Convert Color" },
+    },
+  },
+
   {
     "folke/tokyonight.nvim",
     priority = 1000,
