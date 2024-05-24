3a60bff5264f5e29a219ab6961c9ef8d9a9ece90
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-24 17:26:25 -0600

    Fix auto-pairs generating duplicate parentheses

--- lua/editor/editing.lua
+++ lua/editor/editing.lua
@@ -26,13 +26,7 @@ return {
     "windwp/nvim-autopairs",
     dependencies = { "hrsh7th/nvim-cmp" },
     event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup({})
-      -- If you want insert `(` after select function or method item
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
+    config = function() require("nvim-autopairs").setup({}) end,
   },
 
   -- Auto-saving
