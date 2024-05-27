a9ec3651ac5312c7a16c3faa07cd41809d204059
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-27 14:20:04 -0600

    Update image.nvim config
    but it is still not working

--- lua/ui/image-viewer.lua
+++ lua/ui/image-viewer.lua
@@ -1,11 +1,12 @@
 return {
   "3rd/image.nvim",
   dependencies = { "luarocks.nvim" },
-
   config = function()
     require("image").setup({
       -- ISSUE: Not working
       backend = "ueberzug",
+      max_height_window_percentage = 50,
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
     })
   end,
 }
