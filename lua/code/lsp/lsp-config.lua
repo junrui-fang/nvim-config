578d0e8272da3a79f5722918c2c4f58e767b5da4
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-27 13:20:59 -0600

    Fix lua_ls setup

--- lua/code/lsp/lsp-config.lua
+++ lua/code/lsp/lsp-config.lua
@@ -9,40 +9,33 @@ return {
 
     opts = {
       servers = {
-        lua_ls = { -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
-          on_init = function(client)
-            local path = client.workspace_folders[1].name
-            if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then return end
-
-            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+        lua_ls = { -- https://luals.github.io/wiki/configuration/#neovim
+          settings = {
+            Lua = {
               runtime = {
-                -- Tell the language server which version of Lua you're using
-                -- (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
+                version = "Lua 5.3",
+                path = {
+                  "?.lua",
+                  "?/init.lua",
+                  vim.fn.expand("~/.luarocks/share/lua/5.3/?.lua"),
+                  vim.fn.expand("~/.luarocks/share/lua/5.3/?/init.lua"),
+                  "/usr/share/5.3/?.lua",
+                  "/usr/share/lua/5.3/?/init.lua",
+                },
               },
-              -- Make the server aware of Neovim runtime files
               workspace = {
                 checkThirdParty = false,
                 library = {
-                  vim.env.VIMRUNTIME,
-                  -- Depending on the usage, you might want to add additional paths here.
-                  -- "${3rd}/luv/library"
-                  -- "${3rd}/busted/library",
+                  vim.fn.expand("~/.luarocks/share/lua/5.3"),
+                  "/usr/share/lua/5.3",
                 },
-                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                -- library = vim.api.nvim_get_runtime_file("", true)
               },
-            })
-          end,
-
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
+              completion = { callSnippet = "Replace" },
               codeLens = { enable = true },
-              hint = { enable = true, arrayIndex = "Disable" },
-              -- diagnostics = { disable = { "missing-fields" } },
+              hint = {
+                enable = true,
+                arrayIndex = "Disable",
+              },
             },
           },
         },
