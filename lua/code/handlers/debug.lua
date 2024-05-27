f206bba49e93de72af400af6128e0487ed813e69
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-26 19:38:04 -0600

    Update dap-ui

--- lua/code/handlers/debug.lua
+++ lua/code/handlers/debug.lua
@@ -44,32 +44,29 @@ return {
       )
       vim.keymap.set("n", "<leader>'u", dapui.toggle, { desc = "UI" })
 
-      -- UI setup
-      dapui.setup()
+      -- Dap UI setup
+      dapui.setup({
+        icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+        controls = {
+          icons = {
+            pause = "⏸",
+            play = "▶",
+            step_into = "⏎",
+            step_over = "⏭",
+            step_out = "⏮",
+            step_back = "b",
+            run_last = "▶▶",
+            terminate = "⏹",
+            disconnect = "⏏",
+          },
+        },
+      })
 
       -- Open and close UI automatically
       dap.listeners.before.attach.dapui_config = function() dapui.open() end
       dap.listeners.before.launch.dapui_config = function() dapui.open() end
-      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
-
-      -- Dap UI setup
-      -- dapui.setup({
-      -- 	icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-      -- 	controls = {
-      -- 		icons = {
-      -- 			pause = "⏸",
-      -- 			play = "▶",
-      -- 			step_into = "⏎",
-      -- 			step_over = "⏭",
-      -- 			step_out = "⏮",
-      -- 			step_back = "b",
-      -- 			run_last = "▶▶",
-      -- 			terminate = "⏹",
-      -- 			disconnect = "⏏",
-      -- 		},
-      -- 	},
-      -- })
+      -- dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+      -- dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
 
       -- Install golang specific config
       -- require("dap-go").setup()
