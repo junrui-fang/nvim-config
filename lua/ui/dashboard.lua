88c1c9fb9e64262d6e0863e3cdbf53ab79e1a936
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-05-31 14:19:46 -0600

    Refactor alpha.nvim

--- lua/ui/dashboard.lua
+++ lua/ui/dashboard.lua
@@ -1,67 +1,65 @@
+local header = {
+  [[                                                                       ]],
+  [[                                                                       ]],
+  [[                                                                       ]],
+  [[                                                                       ]],
+  [[                                                                     ]],
+  [[       ████ ██████           █████      ██                     ]],
+  [[      ███████████             █████                             ]],
+  [[      █████████ ███████████████████ ███   ███████████   ]],
+  [[     █████████  ███    █████████████ █████ ██████████████   ]],
+  [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+  [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+  [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+  [[                                                                       ]],
+  [[                                                                       ]],
+  [[                                                                       ]],
+}
+
 return {
-  "goolord/alpha-nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
-  event = "VimEnter",
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
+    -- event = "VimEnter",
+    config = function()
+      local dashboard = require("alpha.themes.dashboard")
+      -- Logo
+      dashboard.section.header.val = header
+      -- Buttons
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+        dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+        dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+        dashboard.button("g", "󱄽 " .. " Grep text", "<cmd> Telescope live_grep <cr>"),
+        dashboard.button("s", " " .. " Session Restore ", [[<cmd> lua require("persistence").load() <cr>]]),
+        dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+        dashboard.button("p", " " .. " Plugins", "<cmd> Lazy <cr>"),
+        dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+      }
+      -- Highlight groups
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      -- Layout
+      dashboard.opts.layout[1].val = 8
+      require("alpha").setup(dashboard.opts)
+    end,
 
-  config = function()
-    local dashboard = require("alpha.themes.dashboard")
-
-    -- Logo
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
-
-    -- Buttons
-    dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
-      dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
-      dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
-      dashboard.button("g", "󱄽 " .. " Grep text", "<cmd> Telescope live_grep <cr>"),
-      dashboard.button("s", " " .. " Session Restore ", [[<cmd> lua require("persistence").load() <cr>]]),
-      dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-      dashboard.button("p", " " .. " Plugins", "<cmd> Lazy <cr>"),
-      dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
-    }
-
-    -- Highlight groups
-    for _, button in ipairs(dashboard.section.buttons.val) do
-      button.opts.hl = "AlphaButtons"
-      button.opts.hl_shortcut = "AlphaShortcut"
-    end
-    dashboard.section.header.opts.hl = "AlphaHeader"
-    dashboard.section.buttons.opts.hl = "AlphaButtons"
-    dashboard.section.footer.opts.hl = "AlphaFooter"
-
-    -- Layout
-    dashboard.opts.layout[1].val = 8
-
-    require("alpha").setup(dashboard.opts)
-  end,
-
-  keys = {
-    { "<leader>sd", "<cmd>Alpha<cr>", desc = "Dashboard" },
-    {
-      "<leader>c",
-      function()
-        local bufs = vim.fn.getbufinfo({ buflisted = 1 })
-        require("utils.buffer").close(0)
-        if not bufs[2] then require("alpha").start() end
-      end,
-      desc = "Close",
+    keys = {
+      { "<leader>sd", "<cmd>Alpha<cr>", desc = "Dashboard" },
+      {
+        "<leader>c",
+        function()
+          local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+          require("utils.buffer").close(0)
+          if not bufs[2] then require("alpha").start() end
+        end,
+        desc = "Close",
+      },
     },
   },
 }
