49ac45ffb3f191a1dcfcc48d6267550650c06a56
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-01 01:48:35 -0600

    Update persistence.nvim to work with barbar.nvim

--- lua/project/session.lua
+++ lua/project/session.lua
@@ -5,11 +5,8 @@ return {
 
     opts = {
       {
-        -- Defaults
-        dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
-        options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
-        pre_save = nil, -- a function to call before saving the session
-        save_empty = false, -- don't save if there are no open file buffers
+        options = { "buffers", "curdir", "tabpages", "winsize", "globals" },
+        pre_save = function() vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" }) end,
       },
     },
 
