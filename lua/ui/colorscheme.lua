996945471205d18dbf6da1b0e8a75f95a733159e
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-01 00:55:31 -0600

    Remove catppuccin colorscheme

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -67,30 +67,4 @@ return {
       },
     },
   },
-
-  {
-    "catppuccin/nvim",
-    lazy = true,
-
-    name = "catppuccin",
-    priority = 1000,
-
-    opts = {
-      transparent_background = true,
-
-      integrations = {
-        -- https://github.com/catppuccin/nvim#integrations
-        -- https://www.lazyvim.org/plugins/colorscheme#catppuccin
-
-        neotree = false,
-
-        noice = false,
-        notify = false,
-
-        which_key = false,
-
-        indent_blankline = { enabled = true },
-      },
-    },
-  },
 }
