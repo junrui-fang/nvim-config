560fe6b6754a0fa9d3dd71a69aaaa2c46b3a0ded
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-02 15:23:44 -0600

    Update comment-box.nvim

--- lua/editor/editing.lua
+++ lua/editor/editing.lua
@@ -24,10 +24,13 @@ return {
     "LudoPinelli/comment-box.nvim",
     keys = {
       { "<leader>;cb", "<cmd>CBccbox<cr>", desc = "Box", mode = { "n", "x" } },
-      { "<leader>;cl", "<cmd>CBllline<cr>", desc = "Line", mode = { "n", "x" } },
-      { "<leader>;cL", "<cmd>CBline<cr>", desc = "Line (Simple)", mode = { "n", "x" } },
+      { "<leader>;ct", "<cmd>CBllline<cr>", desc = "Title Line", mode = { "n", "x" } },
+      { "<leader>;cl", "<cmd>CBline<cr>", desc = "Simple Line", mode = { "n", "x" } },
+      { "<leader>;cm", "<cmd>CBllbox14<cr>", desc = "Marked", mode = { "n", "x" } },
     },
   },
+
+  -- Show case
 
   -- Auto-pairs
   {
