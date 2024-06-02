c65f3623db608cb475a93f1397e5b65f57cdc0f7
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-02 01:43:06 -0600

    Remove useless brackets

--- lua/code/overview/symbol.lua
+++ lua/code/overview/symbol.lua
@@ -1,15 +1,12 @@
 return {
-  {
-    "folke/trouble.nvim",
-    opts = {
-      modes = {
-        symbols = {
-          win = { position = "left", size = 0.17 },
-          focus = true,
-        },
+  "folke/trouble.nvim",
+  opts = {
+    modes = {
+      symbols = {
+        win = { position = "left", size = 0.17 },
+        focus = true,
       },
     },
-
-    keys = { { "<leader>lo", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols" } },
   },
+  keys = { { "<leader>lo", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols" } },
 }
