2c761853a4dc22d07a32ba3dbf222ddf0c62e6cf
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-01 22:46:27 -0600

    Deprecate aerial.nvim

--- lua/code/outline.lua
+++ lua/code/outline.lua
@@ -1,31 +1,10 @@
 return {
   {
-    "stevearc/aerial.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-
     opts = {
-      backends = { "treesitter", "lsp", "markdown", "man" },
-      attach_mode = "global",
-      show_guides = true,
-      filter_kind = false,
-      icons = require("utils.icons").kinds,
-      guides = {
-        mid_item = "├╴",
-        last_item = "└╴",
-        nested_top = "│ ",
-        whitespace = "  ",
-      },
-      layout = {
-        min_width = 30,
-        default_direction = "prefer_left",
       },
     },
 
     keys = {
-      { "<leader>lo", "<cmd>AerialToggle!<CR>", desc = "Outline" },
     },
   },
 }
