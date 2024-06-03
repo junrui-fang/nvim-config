6ff0e786096a2897704b68e7f078cdf984746ea7
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-03 16:18:59 -0600

    Install Hypersonic to deal with regex

---
+++ lua/editor/regex.lua
@@ -0,0 +1,7 @@
+return {
+  "tomiis4/Hypersonic.nvim",
+  event = "CmdlineEnter",
+  cmd = "Hypersonic",
+  opts = {},
+  keys = { { "<leader>;R", ":'<,'>Hypersonic<cr>", mode = "x", desc = "Regex" } },
+}
