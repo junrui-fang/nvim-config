3003b2d10c4584582e346b88cbebacf340c16941
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-02 19:05:42 -0600

    Delete blank lines

--- lua/code/lang/markdown.lua
+++ lua/code/lang/markdown.lua
@@ -11,15 +11,12 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     build = function() vim.fn["mkdp#util#install"]() end,
-
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-
     config = function()
       vim.g.mkdp_auto_close = 1
       vim.g.mkdp_theme = "dark"
     end,
-
     keys = {
       { "<leader>m", "<cmd>MarkdownPreview<cr>", ft = "markdown", desc = "Markdown Preview" },
     },
