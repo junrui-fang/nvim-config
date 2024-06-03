50afdf8a6ee7d1af98e945bcbb1e474add0e2a02
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-03 15:59:13 -0600

    Move spectre to its own file

--- lua/editor/editing.lua
+++ lua/editor/editing.lua
@@ -51,40 +51,4 @@ return {
     },
     keys = { { "<leader>sta", "<cmd>ASToggle<cr>", desc = "Auto-save" } },
   },
-
-  -- Search & Replace
-  {
-    "nvim-pack/nvim-spectre",
-    build = false,
-
-    cmd = "Spectre",
-
-    opts = {
-      -- open_cmd = "noswapfile vnew",
-    },
-
-    keys = {
-      {
-        "<leader>;r",
-        '<cmd>lua require("spectre").toggle()<cr>',
-        desc = "Replace",
-      },
-      {
-        "<leader>;w",
-        '<cmd>lua require("spectre").open_visual({select_word=true})<cr>',
-        desc = "Replace current word",
-      },
-      {
-        "<leader>;w",
-        '<cmd>lua require("spectre").open_visual({select_word=true})<cr>',
-        mode = "v",
-        desc = "Replace current word",
-      },
-      {
-        "<leader>;p",
-        '<cmd>lua require("spectre").open_file_search({select_word=true})<cr>',
-        desc = "Replace on current file",
-      },
-    },
-  },
 }
