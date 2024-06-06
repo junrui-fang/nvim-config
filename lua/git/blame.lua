783ed3e95c7ee46648ba64848baa89bb3dc05027
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-06 17:04:47 -0600

    Use gitlinker to open link to blame

--- lua/git/blame.lua
+++ lua/git/blame.lua
@@ -1,6 +1,17 @@
 return {
-  "FabijanZulj/blame.nvim",
-  cmd = "BlameToggle",
-  opts = {},
-  keys = { { "<leader>gbb", "<cmd>BlameToggle<cr>", desc = "Blame Panel" } },
+  {
+    "FabijanZulj/blame.nvim",
+    cmd = "BlameToggle",
+    opts = {},
+    keys = { { "<leader>gbb", "<cmd>BlameToggle<cr>", desc = "Blame Panel" } },
+  },
+
+  -- Link to blame
+  {
+    "linrongbin16/gitlinker.nvim",
+    keys = {
+      { "<leader>gbl", "<cmd>GitLink blame<cr>", mode = { "n", "v" }, desc = "Yank blame link" },
+      { "<leader>gbL", "<cmd>GitLink! blame<cr>", mode = { "n", "v" }, desc = "Open blame link" },
+    },
+  },
 }
