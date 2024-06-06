239c3e032322d3b79dd2c2221f37deb4cf2682fe
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-05 22:15:18 -0600

    Update zen-mode

--- lua/ui/zen.lua
+++ lua/ui/zen.lua
@@ -1,12 +1,6 @@
 return {
-  {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-
-    opts = {},
-
-    keys = {
-      { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
-    },
-  },
+  "folke/zen-mode.nvim",
+  cmd = "ZenMode",
+  opts = { window = { width = function() return math.min(120, vim.o.columns * 0.75) end } },
+  keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
 }
