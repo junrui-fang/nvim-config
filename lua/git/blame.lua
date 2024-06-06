0397a945c832cb8b72b4a15360b422a9901e9c70
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-06 15:19:33 -0600

    Revert "Install git-blame.nvim to display blame info in virtual text"
    This reverts commit b6fa90ed489457d027cc42a22adc1df374c5404d.

--- lua/git/blame.lua
+++ lua/git/blame.lua
@@ -1,18 +1,6 @@
 return {
-  {
-    "FabijanZulj/blame.nvim",
-    cmd = "BlameToggle",
-    opts = {},
-    keys = { { "<leader>gbb", "<cmd>BlameToggle<cr>", desc = "Blame" } },
-  },
-
-  -- Display blame info as virtual text
-  {
-    "f-person/git-blame.nvim",
-    opts = {
-      enabled = false, -- on startup
-      schedule_event = "CursorHold", -- better performance
-    },
-    keys = { { "<leader>gbt", "<cmd>GitBlameToggle<cr>", desc = "Toggle Virtual Text" } },
-  },
+  "FabijanZulj/blame.nvim",
+  cmd = "BlameToggle",
+  opts = {},
+  keys = { { "<leader>gbb", "<cmd>BlameToggle<cr>", desc = "Blame" } },
 }
