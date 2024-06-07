efda6715b19021ed1a9dc60d1955852ea452cfa2
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-06 22:51:01 -0600

    Update nvim-lint settings to start linting immediately

--- lua/code/handlers/lint.lua
+++ lua/code/handlers/lint.lua
@@ -8,12 +8,13 @@ return {
         markdown = { "markdownlint" },
         html = { "htmlhint" },
       }
-
       -- disable the default linters
       -- lint.linters_by_ft['markdown'] = nil
       -- lint.linters_by_ft['text'] = nil
 
-      -- lint on the specified events.
+      -- start linter immediately
+      lint.try_lint()
+      -- then lint on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         group = lint_augroup,
