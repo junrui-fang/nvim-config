edb9c5d4f093138ae25ffacf358fc215f9d9559c
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-07 00:16:08 -0600

    Update image-viewer

--- lua/ui/image-viewer.lua
+++ lua/ui/image-viewer.lua
@@ -2,11 +2,9 @@ return {
   "3rd/image.nvim",
   dependencies = { "vhyrro/luarocks.nvim", opts = { rocks = { "magick" } } },
   ft = { "markdown", "norg" },
-  config = function()
-    require("image").setup({
-      backend = "kitty",
-      max_height_window_percentage = 50,
-      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
-    })
-  end,
+  opts = {
+    backend = "kitty",
+    max_height_window_percentage = 50,
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg", "*.avif" },
+  },
 }
