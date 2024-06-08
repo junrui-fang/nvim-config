353bb432b7b6e424d3d503ca0e08b156a33b0e71
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-07 23:26:36 -0600

    Format project.nvim

--- lua/project/manager.lua
+++ lua/project/manager.lua
@@ -2,17 +2,12 @@ return {
   {
     "ahmedkhalf/project.nvim",
     event = "VeryLazy",
-
     opts = {
       manual_mode = false, -- Auto switch cwd
       silent_chdir = true,
       ignore_lsp = { "lua_ls" },
     },
-
     config = function(_, opts) require("project_nvim").setup(opts) end,
-
-    keys = {
-      { "<leader>pf", "<cmd>Telescope projects<cr>", desc = "Find Projects" },
-    },
+    keys = { { "<leader>pf", "<cmd>Telescope projects<cr>", desc = "Find Projects" } },
   },
 }
