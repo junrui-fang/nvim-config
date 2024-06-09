842a6f1df93a2ecc99f0d76da90083d16d726d50
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-09 16:36:37 -0600

    Remove tokyonight

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -32,21 +32,6 @@ return {
   },
 
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    priority = 1000,
-    -- init = function() vim.cmd.colorscheme("tokyonight-moon") end,
-    opts = {
-      style = "moon",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-
-  {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
     init = function() vim.cmd.colorscheme("onedark_dark") end,
