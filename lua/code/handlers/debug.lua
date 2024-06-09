daf25f6f9e2901a1ff70d6fac030c226dc9170a6
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-09 16:34:59 -0600

    Update nvim-dap config

--- lua/code/handlers/debug.lua
+++ lua/code/handlers/debug.lua
@@ -10,39 +10,25 @@ return {
       -- Mason
       "williamboman/mason.nvim",
       "jay-babu/mason-nvim-dap.nvim",
+      -- Helper
+      {
+        "Weissle/persistent-breakpoints.nvim",
+        config = function() require("persistent-breakpoints").setup({ load_breakpoints_event = { "BufReadPost" } }) end,
+      },
       -- Debuggers
       -- "leoluz/nvim-dap-go",
     },
 
     config = function()
       local dap, dapui = require("dap"), require("dapui")
-
       require("mason-nvim-dap").setup({
         automatic_installation = true,
-
         handlers = {},
-
         ensure_installed = {
           "python",
           -- "delve",
         },
       })
-
-      vim.keymap.set("n", "<leader>,,", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-      vim.keymap.set("n", "<leader>,c", dap.continue, { desc = "Continue" })
-      vim.keymap.set("n", "<leader>,;", dap.run_to_cursor, { desc = "Run to Cursor" })
-      vim.keymap.set("n", "<leader>,l", dap.run_last, { desc = "Run Last" })
-      vim.keymap.set("n", "<leader>,i", dap.step_into, { desc = "Step Into" })
-      vim.keymap.set("n", "<leader>,o", dap.step_out, { desc = "Step Out" })
-      vim.keymap.set("n", "<leader>,O", dap.step_over, { desc = "Step Over" })
-      vim.keymap.set("n", "<leader>,b", dap.step_back, { desc = "Step Back" })
-      vim.keymap.set(
-        "n",
-        "<leader>,<",
-        function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
-        { desc = "Set Breakpoint" }
-      )
-      vim.keymap.set("n", "<leader>,u", dapui.toggle, { desc = "UI" })
 
       -- Dap UI setup
       dapui.setup({
@@ -61,7 +47,6 @@ return {
           },
         },
       })
-
       -- Open and close UI automatically
       dap.listeners.before.attach.dapui_config = function() dapui.open() end
       dap.listeners.before.launch.dapui_config = function() dapui.open() end
@@ -70,6 +55,43 @@ return {
 
       -- Install golang specific config
       -- require("dap-go").setup()
+    end,
+
+    keys = function()
+      local dap, dapui = require("dap"), require("dapui")
+      local keys = {
+        {
+          "<leader>,,",
+          "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
+          desc = "Toggle Breakpoint",
+        },
+        {
+          "<leader>,<",
+          "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<cr>",
+          desc = "Conditional Breakpoint",
+        },
+        {
+          "<leader>,C",
+          "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>",
+          desc = "Clear Breakpoints",
+        },
+        { "<leader>,c", function() dap.continue() end, desc = "Continue" },
+        { "<leader>,;", function() dap.run_to_cursor() end, desc = "Run to Cursor" },
+        { "<leader>,l", function() dap.run_last() end, desc = "Run Last" },
+        { "<leader>,i", function() dap.step_into() end, desc = "Step Into" },
+        { "<leader>,o", function() dap.step_out() end, desc = "Step Out" },
+        { "<leader>,O", function() dap.step_over() end, desc = "Step Over" },
+        { "<leader>,g", function() dap.goto_() end, desc = "Go to Line" },
+        { "<leader>,j", function() dap.down() end, desc = "Down" },
+        { "<leader>,k", function() dap.up() end, desc = "Up" },
+        { "<leader>,p", function() dap.pause() end, desc = "Pause" },
+        { "<leader>,r", function() dap.repl.toggle() end, desc = "Toggle REPL" },
+        { "<leader>,s", function() dap.session() end, desc = "Session" },
+        { "<leader>,t", function() dap.terminate() end, desc = "Terminate" },
+        { "<leader>,u", function() dapui.toggle() end, desc = "UI" },
+        { "<leader>,w", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+      }
+      return keys
     end,
   },
 
@@ -81,33 +103,6 @@ return {
       { "<leader>,fb", "<cmd>Telescope dap list_breakpoints<cr>", desc = "Breakpoints" },
       { "<leader>,fv", "<cmd>Telescope dap variables<cr>", desc = "Variables" },
       { "<leader>,ff", "<cmd>Telescope dap frames<cr>", desc = "Frames" },
-    },
-  },
-
-  {
-    "Weissle/persistent-breakpoints.nvim",
-    event = "BufReadPost",
-    config = function()
-      require("persistent-breakpoints").setup({
-        load_breakpoints_event = { "BufReadPost" },
-      })
-    end,
-    keys = { -- Overwriting nvim-dap's keymaps
-      {
-        "<leader>,,",
-        "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
-        desc = "Toggle Breakpoint",
-      },
-      {
-        "<leader>,<",
-        "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<cr>",
-        desc = "Set Breakpoint",
-      },
-      {
-        "<leader>,C",
-        "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>",
-        desc = "Clear Breakpoints",
-      },
     },
   },
 }
