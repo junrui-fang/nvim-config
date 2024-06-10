383aeabc7f44c11fe1f45500e6cb22893028fe38
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-09 22:47:40 -0600

    Update lualine in favor of neofusion theme

--- lua/ui/lines/status-line.lua
+++ lua/ui/lines/status-line.lua
@@ -5,48 +5,48 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     event = "VeryLazy",
-
-    opts = {
-      options = {
-        theme = "auto",
-        section_separators = "",
-        -- component_separators = "",
-
-        globalstatus = true,
-        disabled_filetypes = {
-          statusline = { "dashboard" },
-          winbar = {},
-        },
-      },
-
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "diff", "diagnostics", "overseer" },
-        lualine_x = {
-          "filetype",
-          {
-            "swenv",
-            icon = "📦",
-            cond = function() return vim.bo.filetype == "python" end,
+    opts = function()
+      local custom_theme = require("neofusion.lualine")
+      local opts = {
+        options = {
+          theme = custom_theme,
+          section_separators = "",
+          -- component_separators = "",
+          globalstatus = true,
+          disabled_filetypes = {
+            statusline = { "dashboard" },
+            winbar = {},
           },
         },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "diff", "diagnostics" },
-        lualine_x = { "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = { "lazy", "neo-tree" },
-    },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = { "diff", "diagnostics", "overseer" },
+          lualine_x = {
+            "filetype",
+            {
+              "swenv",
+              icon = "📦",
+              cond = function() return vim.bo.filetype == "python" end,
+            },
+          },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = { "diff", "diagnostics" },
+          lualine_x = { "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = { "lazy", "neo-tree" },
+      }
+      return opts
+    end,
   },
 }
