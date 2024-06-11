5e7e95106351ed0f3a9c19cbbdfa1887dc4c82a9
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-10 19:59:49 -0600

    Add more keymaps to neotest

--- lua/code/handlers/test.lua
+++ lua/code/handlers/test.lua
@@ -23,8 +23,14 @@ return {
 
     keys = {
       { "<leader>''", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-      { "<leader>'\"", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
+      {
+        "<leader>'\"",
+        function() require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" }) end,
+        desc = "Run File DAP",
+      },
+      { "<leader>'a", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
       { "<leader>'r", function() require("neotest").run.run() end, desc = "Run Nearest" },
+      { "<leader>'R", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Run Nearest DAP" },
       { "<leader>'l", function() require("neotest").run.run_last() end, desc = "Run Last" },
       { "<leader>'s", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
       {
