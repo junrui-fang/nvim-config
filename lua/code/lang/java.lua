6ec16d46fd2b7e81e48a3e3d6fc9c95e7dbb7db6
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-10 21:02:10 -0600

    Format java.lua

--- lua/code/lang/java.lua
+++ lua/code/lang/java.lua
@@ -2,15 +2,8 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- make sure mason installs the server
-      servers = {
-        jdtls = {},
-      },
-      setup = {
-        jdtls = function()
-          return true -- avoid duplicate servers
-        end,
-      },
+      servers = { jdtls = {} },
+      setup = { jdtls = function() return true end },
     },
   },
 
