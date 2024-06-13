334165262bc5a349708b9e9f42ce6a60e0e4d7a9
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-12 23:18:26 -0600

    Add two links about advanced nvim-lint configurations

--- lua/code/handlers/lint.lua
+++ lua/code/handlers/lint.lua
@@ -1,3 +1,6 @@
+-- INFO: Advanced Configurations:
+-- https://github.com/LazyVim/LazyVim/blob/8346fa7ddc9390312408be571c6443c0b57b9f21/lua/lazyvim/plugins/linting.lua
+-- https://github.com/AstroNvim/astrocommunity/blob/d383aaf072bc265d131b0b2c76a6df7055e14c1f/lua/astrocommunity/lsp/nvim-lint/init.lua#L5
 return {
   {
     "mfussenegger/nvim-lint",
