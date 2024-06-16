f69fcfaf19a34e387ea6db9b0a158d6087ebadc6
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-16 16:26:07 -0600

    Add additional code acitons for go

--- lua/code/lang/go.lua
+++ lua/code/lang/go.lua
@@ -53,4 +53,15 @@ return {
       },
     },
   },
+
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.code_actions.gomodifytags,
+        nls.builtins.code_actions.impl,
+      })
+    end,
+  },
 }
