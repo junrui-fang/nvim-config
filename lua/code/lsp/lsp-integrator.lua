d862f1fc208b64a351167babf6a3367994bdcd74
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-16 16:18:16 -0600

    Setup none-ls, ready for integrating more code actions

--- lua/code/lsp/lsp-integrator.lua
+++ lua/code/lsp/lsp-integrator.lua
@@ -1,24 +1,5 @@
 return {
   "nvimtools/none-ls.nvim",
-  enabled = false,
-
-  config = function()
-    local null_ls = require("null-ls")
-
-    null_ls.setup({
-      sources = {
-        -- Formatters
-        null_ls.builtins.formatting.stylua, -- lua
-        null_ls.builtins.formatting.black, -- Python
-        null_ls.builtins.formatting.isort, -- Python
-        null_ls.builtins.formatting.prettier, -- formatter for many types of files
-
-        null_ls.builtins.completion.spell,
-      },
-    })
-  end,
-
-  keys = {
-    { "<leader>af", vim.lsp.buf.format, desc = "Format" },
-  },
+  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  opts = {},
 }
