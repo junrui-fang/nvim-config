2b516690242cd29e47faca7e7747a7ef7960734e
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-22 19:43:07 -0600

    Setup ruby formatter

--- lua/code/handlers/format.lua
+++ lua/code/handlers/format.lua
@@ -14,6 +14,7 @@ return {
       formatters_by_ft = {
         go = { "goimports", "gofumpt" },
         lua = { "stylua" },
+        ruby = { "standardrb" },
         python = { "isort", "black" }, -- can also run multiple formatters sequentially
         sh = { "shfmt" },
 
