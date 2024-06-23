b2dedd8e2629607b33baddc1f92e8fe25ead3d51
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-23 17:09:35 -0600

    Install nvim-ansible

--- lua/code/lang/ansible.lua
+++ lua/code/lang/ansible.lua
@@ -3,4 +3,18 @@ return {
     "neovim/nvim-lspconfig",
     opts = { servers = { ansiblels = {} } },
   },
+
+  {
+    "mfussenegger/nvim-ansible",
+    ft = "yaml",
+    keys = {
+      {
+        "<leader>la",
+        function() require("ansible").run() end,
+        desc = "Ansible Run Playbook/Role",
+        silent = true,
+        ft = "yaml",
+      },
+    },
+  },
 }
