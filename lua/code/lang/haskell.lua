483dd22783b40da33715e793b8ea135d9eef8657
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-23 23:43:21 -0600

    Install haskell snippets

--- lua/code/lang/haskell.lua
+++ lua/code/lang/haskell.lua
@@ -11,4 +11,14 @@ return {
       if ok then telescope.load_extension("ht") end
     end,
   },
+
+  {
+    "mrcjkb/haskell-snippets.nvim",
+    dependencies = "L3MON4D3/LuaSnip",
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+    config = function()
+      local haskell_snippets = require("haskell-snippets").all
+      require("luasnip").add_snippets("haskell", haskell_snippets, { key = "haskell" })
+    end,
+  },
 }
