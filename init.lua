77add69f9c8e24791c5bbc70ee257a4da4356228
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-24 22:22:33 -0600

    Update lazy.nvim's installation code

--- init.lua
+++ init.lua
@@ -1,16 +1,10 @@
 require("config")
 
--- Install lazy.nvim as the plugin manager
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
