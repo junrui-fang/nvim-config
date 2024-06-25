095a0f234195058a5943b6d1ca1cdf24792395aa
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-25 01:51:30 -0600

    Install vimtex

--- lua/code/lang/tex.lua
+++ lua/code/lang/tex.lua
@@ -3,4 +3,16 @@ return {
     "neovim/nvim-lspconfig",
     opts = { servers = { texlab = {} } },
   },
+
+  {
+    "lervag/vimtex",
+    lazy = false,
+    config = function()
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+    end,
+    keys = {
+      { "<localLeader>l", "", desc = "+vimtext" },
+    },
+  },
 }
