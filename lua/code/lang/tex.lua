0ec16a21da658ba96cafb19af0e6f1c8f3e5015c
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-06-25 17:17:11 -0600

    Setup pdf viewer for vimtex

--- lua/code/lang/tex.lua
+++ lua/code/lang/tex.lua
@@ -10,6 +10,11 @@ return {
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+
+      -- Setup pdf-viewer
+      vim.g.vimtex_view_method = "skim" -- skim for macos
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_reading_bar = 1
     end,
     keys = {
       { "<localLeader>l", "", desc = "+vimtext" },
