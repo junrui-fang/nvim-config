326812778b1be690055197fadd63cef56ec1eb74
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-07-23 03:51:28 -0600

    Add luarocks.nvim as dependency of image.nvim

--- lua/ui/image-viewer.lua
+++ lua/ui/image-viewer.lua
@@ -1,5 +1,12 @@
 return {
   "3rd/image.nvim",
+  dependencies = {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
+    },
+  },
   ft = { "markdown", "norg" },
   opts = {
     backend = "kitty",
