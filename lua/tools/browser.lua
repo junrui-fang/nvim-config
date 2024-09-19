948a52084ecc0fc9faac21e96538b3f03a3a36d7
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2024-09-19 12:58:07 -0600

    Disable fire.nvim

--- lua/tools/browser.lua
+++ lua/tools/browser.lua
@@ -2,6 +2,7 @@ return {
   -- Turn browser into a Neovim client
   {
     "glacambre/firenvim",
+    enabled = false,
     lazy = not vim.g.started_by_firenvim,
     build = function() vim.fn["firenvim#install"](0) end,
   },
