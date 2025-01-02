2b8ca234b5d44f9627b3e29bb9e3f5ec20fc0b92
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2025-01-01 21:40:31 -0700

    Install leetcode.nvim

--- lua/integration/browser.lua
+++ lua/integration/browser.lua
@@ -6,4 +6,17 @@ return {
     lazy = not vim.g.started_by_firenvim,
     build = function() vim.fn["firenvim#install"](0) end,
   },
+
+  -- Leetcode
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    cmd = "Leet",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {},
+  },
 }
