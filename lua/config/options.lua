0d1c35b29d4a77ab6a53ba8c57d83fa9cfd25b92
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2025-04-02 12:11:22 -0400

    update options according to kickstart.nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
--- leader key
+-- keymap options
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time
 
 -- coding options
 vim.opt.shiftwidth = 2
@@ -11,10 +12,12 @@ vim.opt.breakindent = true
 -- editing options
 vim.opt.autowrite = true
 vim.opt.writebackup = true
-vim.opt.undofile = true
+vim.opt.undofile = true -- save undo history
 vim.opt.smartindent = true -- Insert indents automatically
 vim.opt.updatetime = 250 -- Decrease update time
-vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function() vim.opt.clipboard = "unnamedplus" end)
 vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.inccommand = "split" -- preview incremental substitute
 
@@ -25,20 +28,22 @@ vim.opt.splitkeep = "cursor"
 
 -- Search options
 vim.opt.ignorecase = true
-vim.opt.smartcase = true -- Don't ignore case with capitals
+vim.opt.smartcase = true -- won't ignore case with capitals
 
 -- UI options
+vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 0
 vim.opt.cmdheight = 0 -- 0 for noice.nvim
 vim.opt.laststatus = 0 -- disable the status line for lualine
 vim.opt.conceallevel = 1 -- enable for VimTex
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.showmode = false
+vim.opt.showmode = false -- already in the status line
 vim.opt.termguicolors = true -- True color support
-vim.opt.cursorline = true -- Enable highlighting of the current line
+vim.opt.cursorline = true
 vim.opt.wrap = false
 vim.opt.list = true -- Show some invisible characters
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.guicursor = "i:block"
 vim.g.have_nerd_font = true
 vim.opt.fillchars = {
