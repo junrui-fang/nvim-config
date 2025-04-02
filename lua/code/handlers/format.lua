506711a684590ab65b3bdca343c43c04d7bf7fe5
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2025-04-02 19:10:08 -0400

    update conform

--- lua/code/handlers/format.lua
+++ lua/code/handlers/format.lua
@@ -1,14 +1,20 @@
 return {
   {
     "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
         local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+          }
+        end
       end,
 
       formatters_by_ft = {
@@ -59,7 +65,7 @@ return {
     keys = {
       {
         "<leader>;f",
-        function() require("conform").format({ async = true, lsp_fallback = true }) end,
+        function() require("conform").format({ async = true, lsp_format = "fallback" }) end,
         desc = "Format buffer",
       },
     },
