3c23d441686e5316ae71d3a02535f11748ef8b00
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2025-04-03 16:48:27 -0400

    update nvim-lint settings

--- lua/code/handlers/lint.lua
+++ lua/code/handlers/lint.lua
@@ -1,14 +1,11 @@
--- INFO: Advanced Configurations:
--- https://github.com/LazyVim/LazyVim/blob/8346fa7ddc9390312408be571c6443c0b57b9f21/lua/lazyvim/plugins/linting.lua
--- https://github.com/AstroNvim/astrocommunity/blob/d383aaf072bc265d131b0b2c76a6df7055e14c1f/lua/astrocommunity/lsp/nvim-lint/init.lua#L5
 return {
   {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
 
-    opts = {
-      events = { "BufEnter", "BufWritePost", "InsertLeave" },
-      linters_by_ft = {
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
         sh = { "shellcheck" },
         php = { "phpcs" },
         cmake = { "cmakelint" },
@@ -19,16 +16,13 @@ return {
         sql = { "sqlfluff" },
         mysql = { "sqlfluff" },
         plsql = { "sqlfluff" },
-      },
-    },
+      }
 
-    config = function(_, opts)
-      local lint = require("lint")
-      lint.linters_by_ft = opts.linters_by_ft
-
-      vim.api.nvim_create_autocmd(opts.events, {
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-        callback = function() lint.try_lint() end,
+        callback = function()
+          if vim.opt_local.modifiable:get() then lint.try_lint() end
+        end,
       })
     end,
   },
