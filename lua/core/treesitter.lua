97f66a9e1c5822ac611a0c4c227501eff1771f62
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2025-04-02 20:35:04 -0400

    update treesitter

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -13,169 +13,171 @@ return {
     },
     event = "VeryLazy",
 
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+    opts = {
+      ensure_installed = {
+        "lua",
+        "luadoc",
+        "luap",
 
-      ---@diagnostic disable-next-line: missing-fields
-      configs.setup({
-        ensure_installed = {
-          "lua",
-          "luadoc",
-          "luap",
+        "python",
+        "ninja",
+        "rst",
+        "toml",
+        "dap_repl",
 
-          "python",
-          "ninja",
-          "rst",
-          "toml",
-          "dap_repl",
+        "java",
+        "sql",
 
-          "java",
-          "sql",
+        "c",
+        "cpp",
+        "cmake",
+        "objc",
+        "cuda",
+        "proto",
 
-          "c",
-          "cpp",
-          "cmake",
-          "objc",
-          "cuda",
-          "proto",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
 
-          "go",
-          "gomod",
-          "gosum",
-          "gowork",
+        "javascript",
+        "typescript",
+        "tsx",
+        "jsdoc",
+        "html",
+        "css",
+        "scss",
+        "json5",
+        "yaml",
+        "markdown",
+        "markdown_inline",
 
-          "javascript",
-          "typescript",
-          "tsx",
-          "jsdoc",
-          "html",
-          "css",
-          "scss",
-          "json5",
-          "yaml",
-          "markdown",
-          "markdown_inline",
+        "php",
+        "phpdoc",
 
-          "php",
-          "phpdoc",
+        "ruby",
 
-          "ruby",
+        "bash",
+        "dockerfile",
 
-          "bash",
-          "dockerfile",
+        "regex",
+        "latex",
 
-          "regex",
-          "latex",
+        "vim",
+        "vimdoc",
 
-          "vim",
-          "vimdoc",
+        "haskell",
 
-          "haskell",
+        "query",
+        "elixir",
+        "heex",
+      },
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true, disable = { "latex" } },
+      indent = { enable = true },
+      autotag = { enable = true },
 
-          "query",
-          "elixir",
-          "heex",
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<C-s>",
+          node_decremental = "<bs>",
         },
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true, disable = { "latex" } },
-        indent = { enable = true },
-        autotag = { enable = true },
+      },
 
-        incremental_selection = {
+      textobjects = {
+        select = {
           enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+
           keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = "<C-s>",
-            node_decremental = "<bs>",
+            ["aa"] = { query = "@parameter.outer", desc = "around argument" },
+            ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+            ["af"] = { query = "@function.outer", desc = "around function " },
+            ["if"] = { query = "@function.inner", desc = "inside function " },
+            ["ac"] = { query = "@class.outer", desc = "around class" },
+            ["ic"] = { query = "@class.inner", desc = "inside class" },
+
+            ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
+            ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
+            ["al"] = { query = "@loop.outer", desc = "around loop" },
+            ["il"] = { query = "@loop.inner", desc = "inside loop" },
+
+            ["ak"] = { query = "@block.outer", desc = "around block" },
+            ["ik"] = { query = "@block.inner", desc = "inside block" },
+            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+          },
+
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+
+          -- include_surrounding_whitespace = true,
+        },
+
+        move = {
+          enable = true,
+          set_jumps = true,
+
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]a"] = "@parameter.inner",
+            ["]c"] = "@class.outer",
+            ["]l"] = { query = { "@loop.inner", "@loop.outer" }, desc = "Next loop" },
+            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]C"] = "@class.outer",
+            ["]A"] = "@parameter.inner",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[c"] = "@class.outer",
+            ["[a"] = "@parameter.inner",
+            ["[l"] = { query = { "@loop.inner", "@loop.outer" }, desc = "Prev loop" },
+            ["[s"] = { query = "@scope", query_group = "locals", desc = "Prev scope" },
+            ["[z"] = { query = "@fold", query_group = "folds", desc = "Prev fold" },
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[C"] = "@class.outer",
+            ["[a"] = "@parameter.inner",
+          },
+          goto_next = {
+            ["]?"] = { query = "@conditional.outer", desc = "Next conditional" },
+          },
+          goto_previous = {
+            ["[?"] = { query = "@conditional.outer", desc = "Prev conditional" },
           },
         },
 
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-
-            keymaps = {
-              ["aa"] = { query = "@parameter.outer", desc = "around argument" },
-              ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-              ["af"] = { query = "@function.outer", desc = "around function " },
-              ["if"] = { query = "@function.inner", desc = "inside function " },
-              ["ac"] = { query = "@class.outer", desc = "around class" },
-              ["ic"] = { query = "@class.inner", desc = "inside class" },
-
-              ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-              ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-              ["al"] = { query = "@loop.outer", desc = "around loop" },
-              ["il"] = { query = "@loop.inner", desc = "inside loop" },
-
-              ["ak"] = { query = "@block.outer", desc = "around block" },
-              ["ik"] = { query = "@block.inner", desc = "inside block" },
-              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
-            },
-
-            -- include_surrounding_whitespace = true,
+        swap = {
+          enable = true,
+          swap_next = {
+            [">K"] = { query = "@block.outer", desc = "Swap next block" },
+            [">F"] = { query = "@function.outer", desc = "Swap next function" },
+            [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
           },
-
-          move = {
-            enable = true,
-            set_jumps = true,
-
-            goto_next_start = {
-              ["]m"] = { query = "@function.outer", desc = "Next function start" },
-              ["]]"] = { query = "@class.outer", desc = "Next class start" },
-              ["]l"] = { query = { "@loop.inner", "@loop.outer" }, desc = "Next loop" },
-              ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-              ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-              ["[l"] = { query = { "@loop.inner", "@loop.outer" }, desc = "Prev loop" },
-              ["[s"] = { query = "@scope", query_group = "locals", desc = "Prev scope" },
-              ["[z"] = { query = "@fold", query_group = "folds", desc = "Prev fold" },
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-            goto_next = {
-              ["]?"] = { query = "@conditional.outer", desc = "Next conditional" },
-            },
-            goto_previous = {
-              ["[?"] = { query = "@conditional.outer", desc = "Prev conditional" },
-            },
+          swap_previous = {
+            ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+            ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+            ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
           },
-
-          swap = {
-            enable = true,
-            swap_next = {
-              [">K"] = { query = "@block.outer", desc = "Swap next block" },
-              [">F"] = { query = "@function.outer", desc = "Swap next function" },
-              [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
-            },
-            swap_previous = {
-              ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-              ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-              ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-            },
-          },
-
-          -- TODO: https://github.com/nvim-treesitter/nvim-treesitter-textobjects#textobjects-lsp-interop
         },
-      })
 
+        -- TODO: https://github.com/nvim-treesitter/nvim-treesitter-textobjects#textobjects-lsp-interop
+      },
+    },
+
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
       -- make the movements repeatable
       local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
       vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
