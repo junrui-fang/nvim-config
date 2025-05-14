f42c40d354051fbc15f5bf606652152c5ddad542
Junrui Fang <80448002+junrui-fang@users.noreply.github.com>
2025-05-14 09:41:05 -0400

    rename deprecated vim.highlight

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
+  callback = function() vim.hl.on_yank() end,
   desc = "Highlight when yanking text",
 })
 
